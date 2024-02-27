ed6d57394d607645b62443bb2b09354e31c72d8a
alexeydedeshko <905advo@gmai.com>
2024-02-27 18:31:10 +0300

    KS-1-3 do smth

---
+++ src/main/kotlin/lesson_1/lesson1_task5.kt
@@ -0,0 +1,15 @@
+package org.example.lesson_1
+
+fun main() {
+    // var second: Int = 6480
+    // var minute: Int = 108
+    // var hour: Int = 2
+
+    var hour: Int = 1
+    var minute: Int = 48
+    var second: String = "00"
+
+    var notificationText = "01:48:00"
+    println(notificationText)
+
+}
