3b3cfe2ef27cea0958f7fb2ce9b6bfda2f4e5e90
alexeydedeshko <905advo@gmai.com>
2024-02-27 17:52:20 +0300

    KS-1-3 do smth

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,7 +1,18 @@
 package org.example.lesson_1
 
 fun main () {
-    println("Тестовое решение к уроку 1 задаче 1")
-    println("какие то доработки")
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println(year)
+    // println(hour)
+    // println(minute)
+
+    var hourAndMinute = "10:55"
+
+    println(hourAndMinute)
+
+
 }
 
