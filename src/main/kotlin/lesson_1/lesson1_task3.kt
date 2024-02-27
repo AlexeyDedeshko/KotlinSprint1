48db98542576bbced617ec8bfe5c93fd7edfb2e0
alexeydedeshko <905advo@gmai.com>
2024-02-27 17:11:41 +0300

    KS-1-3 do smth

--- src/main/kotlin/lesson_1/lesson1_task3.kt
+++ src/main/kotlin/lesson_1/lesson1_task3.kt
@@ -1,7 +1,8 @@
 package org.example.lesson_1
 
 fun main () {
-    println("Тестовое решение к уроку 1 задаче 1")
-    println("какие то доработки")
-}
+    var numberOfOrders = 75
+    var notificationText = "<Благодарим за покупку и выбор нашего интернет-магазина"
 
+    println(notificationText)
+}
