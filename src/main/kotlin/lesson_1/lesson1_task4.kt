ff67f0c5c2a33aa4af6740243cbaf4269d4d0bf2
alexeydedeshko <905advo@gmai.com>
2024-02-27 18:03:54 +0300

    KS-1-3 do smth

---
+++ src/main/kotlin/lesson_1/lesson1_task4.kt
@@ -0,0 +1,17 @@
+package org.example.lesson_1
+
+fun main () {
+var length: Long = 40_868_600_000
+var age: Byte = 27
+var partOfDay: Double = 0.075
+var second: Int = 6480
+var partOfYearvar: Double = 2.0547945205479453E-4
+var apogee: Int = 327000
+
+    println(length)
+    println(age)
+    println(partOfDay)
+    println(second)
+    println(partOfYearvar)
+    println(apogee)
+}
