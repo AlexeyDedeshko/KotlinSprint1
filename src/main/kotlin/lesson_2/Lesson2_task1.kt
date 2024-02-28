552648a4ce19e3f9bae10c276f9badd21ea4198c
alexeydedeshko <905advo@gmai.com>
2024-02-28 22:06:43 +0300

    KS-2-1 everage score

--- src/main/kotlin/lesson_2/Lesson2_task1.kt
+++ src/main/kotlin/lesson_2/Lesson2_task1.kt
@@ -6,7 +6,7 @@ fun main () {
     val c: Double = 3.00
     val d: Double = 5.00
 
-println((a+b+c+d)/4)
+    println((a+b+c+d)/4)
 
     /*
     Задача состояла в том чтобы вывести два знака после запятой.
