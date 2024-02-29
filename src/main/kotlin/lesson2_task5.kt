de707aa137f662b42fc0f8fbe649e769b1264f87
alexeydedeshko <905advo@gmai.com>
2024-02-29 10:57:31 +0300

    KS-2-5 bankApp

---
+++ src/main/kotlin/lesson2_task5.kt
@@ -0,0 +1,20 @@
+fun main() {
+    val initialDeposit = 70_000.0 // сумма вклада в рублях
+    val annualInterestRate = 0.167 // процентная ставка в качестве доли
+    val years = 20 // продолжительность вклада в годах
+
+    val futureValue = calculateFutureValue(initialDeposit, annualInterestRate, years)
+    val formattedFutureValue = "%.3f".format(futureValue)
+
+    println("Размер вклада через 20 лет: $formattedFutureValue рублей")
+}
+
+fun calculateFutureValue(initialDeposit: Double, annualInterestRate: Double, years: Int): Double {
+    var futureValue = initialDeposit * (1 + annualInterestRate)
+    for (i in 1 until years) {
+        futureValue *= (1 + annualInterestRate)
+    }
+    return futureValue
+}
+
+
