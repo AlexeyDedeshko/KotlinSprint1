545f31c6363c4f7e71411a111c1c0b0be2512521
alexeydedeshko <905advo@gmai.com>
2024-03-01 22:30:31 +0300

    KS-2-3 time of the train arrival

--- src/main/kotlin/lesson2_task3.kt
+++ src/main/kotlin/lesson2_task3.kt
@@ -1,16 +1,19 @@
-fun main () {
+fun main() {
 
     // Исходные данные
-    var exitHourToMin = 9 * 60 + 39 // 9 часов и 39 минут
-    var onTheWay = 457
+    val exitHour = 9
+    val exitMin = 39
+    val onTheWay = 457
+    val minInHour = 60
 
+    // Получаем минуты отбытия
+    val exitTimeToMin = 9 * 60 + 39
     // Складываем минуты отбытия с минутами в пути
-    val exitPlusWay = exitHourToMin + onTheWay
+    val exitPlusWay = exitTimeToMin + onTheWay
 
     // Подсчитываем время прибытия
-    val arrivalHour = exitPlusWay / 60
-    val remainingMin = arrivalHour % 60
-
+    val arrivalHour = exitPlusWay / minInHour
+    val remainingMin = arrivalHour % minInHour
 
     // Форматируем время прибытия с двумя нулями
     val formattedHours = arrivalHour.toString().padStart(2, '0')
@@ -18,9 +21,5 @@ fun main () {
 
     // Выводим результат в консоль
     println("Прибытие поезда в $formattedHours:$formattedMinutes")
-
-
-
-
 
 }
