73b7a9b1b5ad310ede3263dd555aa933958a5ba9
alexeydedeshko <905advo@gmai.com>
2024-03-03 01:36:13 +0300

    KS-2-5-bankApp

--- src/main/kotlin/lesson_2/lesson2_task5.kt
+++ src/main/kotlin/lesson_2/lesson2_task5.kt
@@ -1,29 +1,15 @@
 package lesson_2
+
 import java.lang.StrictMath.pow
+import kotlin.math.pow
+
 fun main() {
     val initialDeposit = 70_000.0 // сумма вклада в рублях
-    val annualInterestRate = 0.167 // процентная ставка в качестве доли
+    val annualInterestRate = 16.7 // процентная ставка
     val years = 20 // продолжительность вклада в годах
 
-    val futureValue = calculateFutureValue(initialDeposit, annualInterestRate, years)
-    val formattedFutureValue = "%.3f".format(futureValue)
-
-    println("Размер вклада через 20 лет: $formattedFutureValue рублей")
-
-    // Функция для возведения в степень
-    val base = 2.0
-    val exponent = 3.0
-    val result = pow(base, exponent)
-    println("$base в степени $exponent равно ${result}")
+    // Формула для расчета через возведение в степень
+    val sum = initialDeposit * (1 + annualInterestRate / 100).pow(years)
+    println("Размер вклада через 20 лет (возведение в степень): $sum")
 
 }
-
-fun calculateFutureValue(initialDeposit: Double, annualInterestRate: Double, years: Int): Double {
-    var futureValue = initialDeposit * (1 + annualInterestRate)
-    for (i in 1 until years) {
-        futureValue *= (1 + annualInterestRate)
-    }
-    return futureValue
-}
-
-
