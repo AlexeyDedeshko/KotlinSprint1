1a8dd474ca058c513789b68b58c855a0be0c330b
alexeydedeshko <905advo@gmai.com>
2024-04-03 23:01:48 +0300

    KS-4-4-training

--- src/main/kotlin/lesson_1/Lesson1_task1.kt
+++
@@ -1,8 +0,0 @@
-package org.example.lesson_1
-
-fun main() {
-    val numberOfOrders = 75
-    val notificationText = "<Благодарим за покупку и выбор нашего интернет-магазина"
-
-    println(notificationText)
-}
