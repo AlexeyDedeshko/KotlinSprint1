e3d62b9f0335b18a8e0fd8befabc78f8312b5a37
alexeydedeshko <905advo@gmai.com>
2024-04-05 22:47:01 +0300

    KS-6-5-auth-update

--- src/main/kotlin/Lesson_6/Lesson6_task5.kt
+++ src/main/kotlin/Lesson_6/Lesson6_task5.kt
@@ -3,6 +3,66 @@ package Lesson_6
 class Lesson6_task5 {
 }
 
+package Lesson_6
+
+fun main() {
+
+    // Создаем функцию для регистрации и авторизации пользователя
+    fun registerAndLogin() {
+        var login: String? = null
+        var password: String? = null
+
+        // Блок программы для создания логина и пароля
+        println("Создайте логин и пароль")
+        while (login == null || login.isEmpty()) {
+            print("Введите логин: ")
+            login = readLine()?.trim()
+            if (login.isNullOrEmpty()) {
+                println("Логин не может быть пустым")
+            }
+        }
+
+        while (password == null || password.isEmpty()) {
+            print("Введите пароль: ")
+            password = readLine()?.trim()
+            if (password.isNullOrEmpty()) {
+                println("Пароль не может быть пустым")
+            }
+        }
+
+        // Блок программы для авторизации пользователя
+        println("Авторизация")
+        var inputLogin: String? = null
+        var inputPassword: String? = null
+
+        while (inputLogin != login || inputPassword != password) {
+            print("Введите логин: ")
+            inputLogin = readLine()?.trim()
+            print("Введите пароль: ")
+            inputPassword = readLine()?.trim()
+
+            if (inputLogin != login || inputPassword != password) {
+                println("Логин или пароль неверны. Попробуйте еще раз.")
+            }
+        }
+
+        // Вывод сообщения об успешной авторизации
+        println("Авторизация прошла успешно")
+    }
+
+// Вызываем функцию для регистрации и авторизации пользователя
+    registerAndLogin()
+
+}
+
+
+/*
+На этапе авторизации в приложении пользователь, не имея аккаунта, выбрал регистрацию. Нужно создать блок программы,
+который сначала предлагает создать логин и пароль, затем запрашивает их, чтобы войти в приложение.
+
+Если логин или пароль введены неверно – данные запрашиваются заново. Если данные верны, выводится сообщение –
+“Авторизация прошла успешно”.
+ */
 /*
 
 Доработка задачи на авторизацию. Нужно написать часть модуля для авторизации пользователя. Для входа в приложение
